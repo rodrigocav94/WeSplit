f5dc7ce0f8cd0c1df665cda4a50debaf5c58808a
Rodrigo A. Cavalcanti <rodrigocav94@gmail.com>
2024-05-21 15:13:03 -0300

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Rodrigo Cavalcanti on 21/05/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
