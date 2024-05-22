62b48d123bae2fe15fd9445ece500c07219fe2a8
Rodrigo A. Cavalcanti <rodrigocav94@gmail.com>
2024-05-22 15:03:35 -0300

    Added percentage tip picker

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -35,11 +35,20 @@ struct ContentView: View {
                     .pickerStyle(.navigationLink)
                 }
                 
+                Section("How much do you want to tip?") {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                
                 Section {
                     Text(
                         checkAmount,
                         format: .currency (
-                        code: Locale.current.currency?.identifier ?? "USD"
+                            code: Locale.current.currency?.identifier ?? "USD"
                         )
                     )
                 }
