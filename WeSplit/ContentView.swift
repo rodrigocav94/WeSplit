62667ede964bc87be7f082a9ff7aa110d24937ba
Rodrigo A. Cavalcanti <rodrigocav94@gmail.com>
2024-05-22 14:43:22 -0300

    Initial State

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,14 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            Form {
+                Section {
+                    TextField (
+                        "Amount",
+                        value: $checkAmount,
+                        format: .currency (
+                            code: Locale.current.currency?.identifier ?? "USD"
+                        )
+                    )
+                    .keyboardType(.decimalPad)
+                    
+                    Picker("Number of People", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
+                    }
+                    .pickerStyle(.navigationLink)
+                }
+                
+                Section {
+                    Text(
+                        checkAmount,
+                        format: .currency (
+                        code: Locale.current.currency?.identifier ?? "USD"
+                        )
+                    )
+                }
+            }
+            .navigationTitle("WeSplit")
         }
-        .padding()
     }
 }
 
