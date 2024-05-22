c25cb95aaa3e99634e05776ba57e1681ac594a31
Rodrigo A. Cavalcanti <rodrigocav94@gmail.com>
2024-05-22 15:18:16 -0300

    Added total amount per person

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -14,6 +14,16 @@ struct ContentView: View {
     
     let tipPercentages = [10, 15, 20, 25, 0]
     
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        return amountPerPerson
+    }
+    
     var body: some View {
         NavigationStack {
             Form {
@@ -46,7 +56,7 @@ struct ContentView: View {
                 
                 Section {
                     Text(
-                        checkAmount,
+                        totalPerPerson,
                         format: .currency (
                             code: Locale.current.currency?.identifier ?? "USD"
                         )
