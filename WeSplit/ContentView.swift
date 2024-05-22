a1fe53c64afa45969b6b45851cca9741420ef0eb
Rodrigo A. Cavalcanti <rodrigocav94@gmail.com>
2024-05-22 15:31:34 -0300

    Added button to dismiss keyboard

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -11,6 +11,7 @@ struct ContentView: View {
     @State private var checkAmount = 0.0
     @State private var numberOfPeople = 2
     @State private var tipPercentage = 20
+    @FocusState private var amountIsFocused: Bool
     
     let tipPercentages = [10, 15, 20, 25, 0]
     
@@ -36,6 +37,7 @@ struct ContentView: View {
                         )
                     )
                     .keyboardType(.decimalPad)
+                    .focused($amountIsFocused)
                     
                     Picker("Number of People", selection: $numberOfPeople) {
                         ForEach(2..<100) {
@@ -64,6 +66,13 @@ struct ContentView: View {
                 }
             }
             .navigationTitle("WeSplit")
+            .toolbar {
+                if amountIsFocused {
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
         }
     }
 }
